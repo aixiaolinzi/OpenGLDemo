8f17a310cdca0b3790b8c9dac5fc8222357e97a1
YueZongzhe <yuezz0926@thundersoft.com>
2020-06-08 14:42:19 +0800

    删除  DameServerDemo

--- DameServerDemo/src/main/java/com/thundersoft/dameserverdemo/InCarMonitorManager.java
+++
@@ -1,7 +0,0 @@
-package com.thundersoft.dameserverdemo;
-
-public final class InCarMonitorManager {
-    public static int FUNCTION_SUCCESS = 0;
-    public static int UNKNOWN_ERROR = -1;
-    public static int FUNCTION_DISABLED = -2;
-}
--- DameServerDemo/src/main/java/com/thundersoft/dameserverdemo/video/core/IObserver.java
+++
@@ -1,8 +0,0 @@
-package com.thundersoft.dameserverdemo.video.core;
-
-
-public interface IObserver<Type> {
-
-    void onCall(Type type);
-
-}
